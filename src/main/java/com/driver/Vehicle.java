0cc845aa8024c0d8722b4aa00629693b63290f4f
poovilikarthika <pooviliselvi.8502gmail.com>
2023-01-21 23:29:03 +0530

    second commit

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -2,6 +2,14 @@ package com.driver;
 
 public class Vehicle {
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     private String name;
 
     public int getCurrentSpeed() {
